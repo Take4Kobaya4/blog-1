7cec847f363fb1633dc7ff6207d370e4259c5540
Take4Kobaya4 <t.kobayashi3927@gmail.com>
2022-06-10 20:56:57 +0900

    バリデーションの作成

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,22 +3,28 @@
     <h2 class="font-semibold text-xl text-grey-800 leading-tight">
       投稿の新規作成
     </h2>
+
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+    <x-message :message="session('message')" />
   </x-slot>
 
 
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="mx-4 sm:p-8">
-        <form enctype="multipart/form-data">
+      {{-- actionの後に{{  }}を入れることでXSS対策ができる --}}
+        <form method="post" action="{{ route('post.store') }}" enctype="multipart/form-data">
+          @csrf
             <div class="md:flex items-center mt-8">
                 <div class="w-full flex flex-col">
                 <label for="body" class="font-semibold leading-none mt-4">件名</label>
-                <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" placeholder="Enter Title">
+                <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{ old('title') }}" placeholder="Enter Title">
                 </div>
             </div>
 
             <div class="w-full flex flex-col">
                 <label for="body" class="font-semibold leading-none mt-4">本文</label>
-                <textarea name="body" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="body" cols="30" rows="10"></textarea>
+                <textarea name="body" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="body" cols="30" rows="10">{{ old('body') }}</textarea>
             </div>
 
             <div class="w-full flex flex-col">
